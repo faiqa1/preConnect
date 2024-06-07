310d4f97fd17bcd91d5a566cad4ae3f50e276b15
faiqa1 <faiqarao03@gmail.com>
2024-06-07 23:45:38 +0500

    Updated SideContainer component

--- client/src/Components/Pages/UserHomePage/SideContainer.js
+++ client/src/Components/Pages/UserHomePage/SideContainer.js
@@ -3,9 +3,9 @@ import React from 'react'
 export default function SideContainer() {
   return (
     <div className='bg-lightblue w-[20%] h-[10rem] float-right'>
-      <h3>blogs</h3>
-      <h3>blogs</h3>
-      <h3>blogs</h3>
+      <h3>Blogs</h3>
+      <h3>Jobs</h3>
+      <h3>Companies</h3>
     </div>
   )
 }
