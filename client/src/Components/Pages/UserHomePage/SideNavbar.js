3f30fb3d46fff14d91263c5e6f7bd1bc69add03d
faiqa1 <faiqarao03@gmail.com>
2024-06-10 14:02:20 +0500

    Updated SideNavbar component to include profile picture, name, description, and 'View Profile' link

--- client/src/Components/Pages/UserHomePage/SideNavbar.js
+++ client/src/Components/Pages/UserHomePage/SideNavbar.js
@@ -1,16 +1,32 @@
 import React from "react";
-import pic from '../../../Images/profilepic.png';
+import { Link } from "react-router-dom";
+import pic from "../../../Images/profilepic.png";
 
 export default function SideNavbar() {
   return (
     <div className="bg-blue w-[20rem] h-[25rem] rounded-lg float-left  mx-8 mt-[-3rem]">
-      <div className="flex justify-center items-center w-[15rem] h-[8rem]  mx-auto my-5">
-        <div className="profileCircle w-[8rem] h-[8rem] rounded-full 
+      <Link to="#">
+        <div className="flex justify-center flex-col items-center w-[19rem] h-[24rem]  mx-auto my-2">
+          <div
+            className="profileCircle w-[8rem] h-[8rem] rounded-full 
         bg-[#0969c3]
-        relative overflow-hidden">
-          <img src={pic} className="absolute w-full h-full object-cover " />
+        relative overflow-hidden"
+          >
+            <img src={pic} className="absolute w-full h-full object-cover " />
+          </div>
+          <div className="textWrapper">
+            <h3 className="text-white font-extrabold text-xl text-center">Becky Hill</h3>
+            <p className="text-white  text-md py-2 px-5 w-[95%]">
+              Node|| React|| MongoDB|| Express ||MERN Stack ||Web
+              Development||Docker ||CI/CD Pipelines
+            </p>
+
+            <Link>
+            <p className="text-white font-extrabold text-xl text-center">View Profile</p>
+            </Link>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
