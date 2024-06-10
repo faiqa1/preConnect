87f2ff10e74c8dfa635307743f9eefb43c7bdedd
faiqa1 <faiqarao03@gmail.com>
2024-06-10 12:55:59 +0500

    Updated SideContainer component: changed text of three h3 elements from 'blogs' to 'Blogs', 'Jobs', and 'Companies'.

--- client/src/Components/Pages/UserHomePage/SideNavbar.js
+++ client/src/Components/Pages/UserHomePage/SideNavbar.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 
 export default function SideNavbar() {
- 
-
   return (
-    <div className="bg-lightblue w-[20%] h-[10rem] float-left">
-   
+    <div className="bg-blue w-[20%] h-[10rem] mx-auto">
+      <div className="flex justify-center items-center w-[19%] h-[8rem] 
+      bg-[#0969c3] mx-auto">
+        <h3>Profile pic</h3>
+      </div>
     </div>
   );
 }
